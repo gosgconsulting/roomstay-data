d2072a2fd6b89d26d2907222d603275318bac5a5
gosgconsulting <contact@gosgconsulting.com>
2025-11-10 03:08:08 +0700

    1

--- supabase/functions/get-performance-data/index.ts
+++ supabase/functions/get-performance-data/index.ts
@@ -2,8 +2,9 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.77.0';
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, DELETE, PATCH',
+  'Access-Control-Allow-Headers': '*',
+  'Access-Control-Max-Age': '86400',
 };
 
 interface PerformanceDataRequest {
